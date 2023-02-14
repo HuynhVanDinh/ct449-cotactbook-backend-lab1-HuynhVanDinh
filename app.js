798759fa4381430bc22f990fcb25bd188644bb05
HuynhVanDinh <dinh0375751606@gmail.com>
2023-02-14 21:25:06 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const ApiError = require("./app/api-error");
 
 const contactsRouter = require("./app/routes/contact.route");
 const app = express();
@@ -12,4 +13,13 @@ app.get("/", (req, res) => {
 });
 app.use("/api/contacts", contactsRouter);
 
+app.use((req, res, next) => {
+  return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((error, req, res, next) => {
+  return res.status(error.statusCode || 500).json({
+    message: error.message || "Internal Server Error",
+  });
+});
 module.exports = app;
